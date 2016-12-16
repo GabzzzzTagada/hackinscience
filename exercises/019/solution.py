df172ee5abd26fd27881ec2c6d6afc334f5c7f46
Gabz <gabriel.ragala@cri-paris.org>
2016-12-16 14:58:19 +0100

    up

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -3,4 +3,4 @@ import sys
 if len(sys.argv) >= 2:
     print(int(sys.argv[1]) + int(sys.argv[2]))
 else:
-    print("usage: python3 solution.py 0P1 0P2")
+    print("usage: python3 solution.py OP1 OP2")
