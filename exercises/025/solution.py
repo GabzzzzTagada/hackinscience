208cd78b8242c637f7e5157bf10c0c09baa811b6
Gabz <gabriel.ragala@cri-paris.org>
2016-12-16 15:48:28 +0100

    up

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -3,4 +3,4 @@ from datetime import datetime
 import time
 x = time.strftime('%Y-%d-%m')
 y = time.strftime('%H-%M-%S')
-print("Today is " + x +  " and it is " + y)
+print("Today is " + x + " and it is " + y)
